18c4d7d07c6d2aaa8b484cf313ca8aa9b5925b26
alejandro <alejandro.arcas.7e5@itb.cat>
2023-10-17 00:49:33 +0200

    BottomNavigation & Fonts (Home page)

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'home_page/home_page_main.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,33 +12,14 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'PetLink',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: const Color.fromRGBO(
+            1, 98, 94, 1.0)),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Home'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
+      home: const MyHomePage(title: Text("Home", style: TextStyle(color: Colors.white, fontFamily: 'BalooDa2', fontSize: 23, fontWeight: FontWeight.w500))),
     );
   }
 }
